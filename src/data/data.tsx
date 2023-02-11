fe641bb8564aba8f540214a0ed61bce2c6cee28f
Ziyang <11910419@mail.sustech.edu.cn>
2023-02-11 15:08:28 +0800

    Update title

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -36,8 +36,8 @@ import {
  * Page meta data
  */
 export const homePageMeta: HomepageMeta = {
-  title: 'React Resume Template',
-  description: "Example site built with Tim Baker's react resume template",
+  title: 'Ziyang Yu\'s homepage',
+  description: "Personal website for Ziyang Yu.",
 };
 
 /**
